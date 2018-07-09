1ea000d830dbe0d4a58040f04ee2154389611b65
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-09 20:12:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    puts "Hash is empty"
+  else
+    puts "Hash is not empty"
+   end 
 
+  name_hash 
 
 
 end
