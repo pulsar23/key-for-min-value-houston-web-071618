73016671a0b4a4fa9876438aaf7dcca2d27d0482
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-09 20:28:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  first_time = 1 
+  
   if name_hash == {} || name_hash == nil
     puts "Hash is empty"
     return
   else
     name_hash.each do |key, value|
-      if first_value == 1 
+      if first_time == 1 
         smallest_value = value
         smallest_key = key
-        first_value = 2
+        first_time = 2
       else
       puts "#{key} => #{value}"
       puts "Hash is not empty"
